b7ea623185a74e737106749bfad8a17b41759b23
vivpandey13 <vivmoney13@hotmail.com>
2019-02-06 01:09:33 +0530

    Add project files.

---
+++ MVC App/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using MVC_App.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_App.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Save(Applicant applicant)
+        {
+            return $"{applicant.ID} = {applicant.Name}";
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MVC App/Models/Applicant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_App.Models
+{
+    public class Applicant
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
